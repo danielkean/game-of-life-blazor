57681cc9708487ff556817f8da0a1d5424fc4ac6
Daniel Kean <64756591+danielkean@users.noreply.github.com>
2021-12-02 18:01:57 +0000

    Implement SVG image and button components

---
+++ Components/Icons/SvgImage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GameOfLife.Components.Icons;
+
+public class SvgImage: ComponentBase
+{
+    [Parameter] public int Size { get; set; } = 24;
+    [Parameter] public float StrokeWidth { get; set; } = 1.5f;
+}
